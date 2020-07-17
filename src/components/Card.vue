4fe5ceeb876fe7bd086324180b4ac81021494891
Max <michalek.m@web.de>
2020-07-17 12:04:23 +0200

    Add Card component and add list of Card components for Characters

---
+++ src/components/Card.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card">
+    <router-link :to="routeOnClick" class="image">
+      <div>
+        <img v-bind:src="thumbnail" alt="Thumbnail of card element" />
+      </div>
+
+      <div class="card-body">
+        <h3>{{ label }}</h3>
+        <ul>
+          <li>{{ itemOne }}</li>
+          <li>{{ itemTwo }}</li>
+          <li>{{ itemThree }}</li>
+        </ul>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    thumbnail: String,
+    label: String,
+    itemOne: String,
+    itemTwo: String,
+    itemThree: String,
+    routeOnClick: String
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  height: 100%;
+
+  background-color: #efefef;
+
+  cursor: pointer;
+
+  border-radius: 2px;
+}
+
+.card-body {
+  text-align: left;
+  padding: 0px 5px 5px 10px;
+}
+
+a {
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+h3 {
+  margin: 0px;
+
+  font-size: 0.8rem;
+}
+
+ul {
+  padding: 0px;
+  margin: 2px 0px 2px 0px;
+}
+
+li {
+  list-style-type: none;
+
+  font-size: 0.6rem;
+}
+
+img {
+  width: 100%;
+
+  border-radius: 2px 2px 0px 0px;
+}
+</style>
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="character">
     <ContentHeader v-bind:mainTitle="this.$options.name" />
+    <div class="container fd-col">
+      <div class="card-gird">
+        <div v-for="(character, index) in characters" v-bind:key="index">
+          <Card
+            v-bind:thumbnail="character.image"
+            v-bind:label="character.name"
+            v-bind:itemOne="`Species: ${character.species}`"
+            v-bind:itemTwo="`Status: ${character.status}`"
+            v-bind:itemThree="`Origin: ${character.origin.name}`"
+            v-bind:routeOnClick="`/characters/details/${character.id}`"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Card from "@/components/Card";
 import ContentHeader from "@/components/ContentHeader.vue";
 
 export default {
   name: "Characters",
   components: {
+    Card,
     ContentHeader
   },
   computed: {
