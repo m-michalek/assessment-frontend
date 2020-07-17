ba6fefa55e4b0ce9ce104ce4f0e9facd3dd96cdb
Max <michalek.m@web.de>
2020-07-17 11:26:42 +0200

    Add basic header component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header />
+    <div>
+      <router-view class="main-content" />
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
 <style>
+html,
+body {
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
+
   color: #2c3e50;
+
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
 }
 
-#nav {
-  padding: 30px;
-}
+.container {
+  height: 100%;
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  margin-left: 20px;
+  margin-right: 20px;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  display: flex;
+  align-items: center;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container justify-content-center logo-sizer">
+      <img src="@/assets/Rick_and_Morty_white.svg" alt="Rick and Morty logo" width="100%" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home"
+};
 </script>
+
+<style scoped>
+.home {
+  background-color: #10172c;
+}
+
+.home > .container > svg {
+  fill: white;
+}
+
+.logo-sizer {
+  height: calc(100vh - 134px);
+}
+</style>
